09d8a67c6f984e513e3f619d44c34a8ed96d487d
Hadi Soufan <hadisoufan74@gmail.com>
2024-02-26 20:12:54 +0200

    Application Db Context

---
+++ PMSApi/Persistence/DataContext.cs
@@ -0,0 +1,13 @@
+﻿using Domain.Appointment;
+using Microsoft.EntityFrameworkCore;
+
+namespace Persistence
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+    }
+}
