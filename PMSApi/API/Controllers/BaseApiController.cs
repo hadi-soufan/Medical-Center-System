6165a7eb1ed2327da3c2acaffb24e89650dbac91
Hadi Soufan <hadisoufan74@gmail.com>
2024-02-28 23:38:50 +0200

    Implementing the BasiApiController

---
+++ PMSApi/API/Controllers/BaseApiController.cs
@@ -0,0 +1,19 @@
+﻿using MediatR;
+
+using Microsoft.AspNetCore.Mvc;
+
+/// <summary>
+/// Represents a base API controller providing access to the mediator for handling requests.
+/// </summary>
+[ApiController]
+[Route("api/[controller]")]
+public class BaseApiController : ControllerBase
+{
+    private IMediator _mediator;
+
+    /// <summary>
+    /// Gets the mediator for handling requests.
+    /// </summary>
+    protected IMediator Mediator => _mediator ??=
+        HttpContext.RequestServices.GetService<IMediator>();
+}
