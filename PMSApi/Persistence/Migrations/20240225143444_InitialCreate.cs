1f8671ea3daa713225ab4d7ace69d8f631907df2
Hadi Soufan <hadisoufan74@gmail.com>
2024-02-28 23:42:37 +0200

    Migrations

---
+++ PMSApi/Persistence/Migrations/20240225143444_InitialCreate.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Appointments",
+                columns: table => new
+                {
+                    AppointmentId = table.Column<Guid>(type: "uuid", nullable: false),
+                    AppointmentDate = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    AppointmentStatus = table.Column<int>(type: "integer", nullable: false),
+                    AppointmentStatusAr = table.Column<int>(type: "integer", nullable: false),
+                    AppointmentType = table.Column<int>(type: "integer", nullable: false),
+                    AppointmentTypeAr = table.Column<int>(type: "integer", nullable: false),
+                    Notes = table.Column<string>(type: "text", nullable: true),
+                    NotesAr = table.Column<string>(type: "text", nullable: true),
+                    CreatedAt = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    UpdatedAt = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    IsCancelled = table.Column<bool>(type: "boolean", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Appointments", x => x.AppointmentId);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Appointments");
+        }
+    }
+}
---
+++ PMSApi/Persistence/Migrations/20240227190609_ModifingAppoitments.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class ModifingAppoitments : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "AppointmentStatusAr",
+                table: "Appointments");
+
+            migrationBuilder.DropColumn(
+                name: "AppointmentTypeAr",
+                table: "Appointments");
+
+            migrationBuilder.DropColumn(
+                name: "NotesAr",
+                table: "Appointments");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "AppointmentStatusAr",
+                table: "Appointments",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "AppointmentTypeAr",
+                table: "Appointments",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<string>(
+                name: "NotesAr",
+                table: "Appointments",
+                type: "text",
+                nullable: true);
+        }
+    }
+}
