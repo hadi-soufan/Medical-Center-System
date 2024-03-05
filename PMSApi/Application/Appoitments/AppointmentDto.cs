48a831490ec5e02453adf42ad788db4adc7211be
Hadi Soufan <hadisoufan74@gmail.com>
2024-03-05 06:09:36 +0200

    Data transfer object for appointment information.

--- PMSApi/Application/Appoitments/AppointmentDto.cs
+++ PMSApi/Application/Appoitments/AppointmentDto.cs
@@ -1,11 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Application.Appoitments
+﻿namespace Application.Appoitments
 {
+    /// <summary>
+    /// Data transfer object for appointment information.
+    /// </summary>
     public class AppointmentDto
     {
         public Guid AppointmentId { get; set; }
