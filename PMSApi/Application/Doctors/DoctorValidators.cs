39871dbc6d1029f9f76a71768314cd231df092a3
Hadi Soufan <hadisoufan74@gmail.com>
2024-03-09 07:07:26 +0200

    Provides validation rules for DoctorDto objects.

---
+++ PMSApi/Application/Doctors/DoctorValidators.cs
@@ -0,0 +1,21 @@
+﻿using FluentValidation;
+
+namespace Application.Doctors
+{
+    /// <summary>
+    /// Provides validation rules for DoctorDto objects.
+    /// </summary>
+    public class DoctorValidators : AbstractValidator<DoctorDto>
+    {
+        public DoctorValidators()
+        {
+            RuleFor(x => x.Email).NotEmpty();
+            RuleFor(x => x.PhoneNumber).NotEmpty();
+            RuleFor(x => x.Address).NotEmpty();
+            RuleFor(x => x.City).NotEmpty();
+            RuleFor(x => x.ZipCode).NotEmpty();
+            RuleFor(x => x.Occupation).NotEmpty();
+            RuleFor(x => x.State).NotEmpty();
+        }
+    }
+}
