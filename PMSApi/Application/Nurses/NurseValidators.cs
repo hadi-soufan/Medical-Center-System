13e0547019a313c3d568a117040ad78e8c14693b
Hadi Soufan <hadisoufan74@gmail.com>
2024-03-09 07:10:25 +0200

    Provides validation rules for the properties of a NurseDto.

---
+++ PMSApi/Application/Nurses/NurseValidators.cs
@@ -0,0 +1,21 @@
+﻿using FluentValidation;
+
+namespace Application.Nurses
+{
+    /// <summary>
+    /// Provides validation rules for the properties of a NurseDto.
+    /// </summary>
+    public class NurseValidators : AbstractValidator<NurseDto>
+    {
+        public NurseValidators()
+        {
+            RuleFor(x => x.Email).NotEmpty().EmailAddress();
+            RuleFor(x => x.PhoneNumber).NotEmpty();
+            RuleFor(x => x.Address).NotEmpty();
+            RuleFor(x => x.City).NotEmpty();
+            RuleFor(x => x.ZipCode).NotEmpty();
+            RuleFor(x => x.Occupation).NotEmpty();
+            RuleFor(x => x.State).NotEmpty();
+        }
+    }
+}
