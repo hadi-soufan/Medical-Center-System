9d95189c350a19f858608bb64cc0cfd48c4b1669
Hadi Soufan <hadisoufan74@gmail.com>
2024-03-09 07:08:47 +0200

    Data transfer object for medical history information.

---
+++ PMSApi/Application/MedicalHistoreis/MedicalHistoryDto.cs
@@ -0,0 +1,51 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Application.MedicalHistoreis
+{
+    /// <summary>
+    /// Data transfer object for medical history information.
+    /// </summary>
+    public class MedicalHistoryDto
+    {
+        public Guid MedicalHistoryId { get; set; }
+
+        public decimal Height { get; set; }
+
+        public decimal Weight { get; set; }
+
+        [StringLength(255)]
+        public string MedicalProblems { get; set; }
+
+        [StringLength(255)]
+        public string MentalHealthProblems { get; set; }
+
+        [StringLength(255)]
+        public string Medicines { get; set; }
+
+        [StringLength(255)]
+        public string Allergics { get; set; }
+
+        [StringLength(255)]
+        public string SugreriesHistory { get; set; }
+
+        [StringLength(255)]
+        public string Vaccines { get; set; }
+
+        [StringLength(255)]
+        public string Diagnosis { get; set; }
+
+        [StringLength(255)]
+        public string TestsPerformed { get; set; }
+
+        [StringLength(255)]
+        public string TreatmenPlans { get; set; }
+
+        [StringLength(255)]
+        public string FamilyMedicalHistory { get; set; }
+
+        public DateOnly CreatedAt { get; set; } = DateOnly.FromDateTime(DateTime.UtcNow);
+        public DateOnly UpdatedAt { get; set; }
+        public bool IsDeleted { get; set; } = false;
+        public string PatientName { get; set; }
+    }
+}
