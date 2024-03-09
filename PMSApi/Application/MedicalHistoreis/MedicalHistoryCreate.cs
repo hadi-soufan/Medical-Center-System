514e7ef8a712b959eb60da6d2b2544b4bfc9bf93
Hadi Soufan <hadisoufan74@gmail.com>
2024-03-09 07:08:17 +0200

    Represents a command to create a new medical history.

--- PMSApi/Application/MedicalHistoreis/MedicalHistoryCreate.cs
+++ PMSApi/Application/MedicalHistoreis/MedicalHistoryCreate.cs
@@ -1,95 +1,75 @@
-﻿    using MediatR;
-    using Persistence;
-    using Domain.Entities;
-    using Application.Appoitments;
-    using FluentValidation;
-    using Application.MedicalHistoreis;
-    using Application.Core;
+﻿using MediatR;
+using Persistence;
+using Domain.Entities;
+using FluentValidation;
+using Application.MedicalHistoreis;
+using Application.Core;
 using Microsoft.EntityFrameworkCore;
 
-    namespace Application.MedicalHistories
+namespace Application.MedicalHistories
+{
+    /// <summary>
+    /// Represents a command to create a new medical history.
+    /// </summary>
+    public class MedicalHistoryCreate
     {
         /// <summary>
-        /// Represents a command to create a new medical history.
+        /// Command to create a new medical history.
         /// </summary>
-        public class MedicalHistoryCreate
+        public class Command : IRequest<Result<Unit>>
         {
-            /// <summary>
-            /// Represents the command to create a new medical history.
-            /// </summary>
-            public class Command : IRequest<Result<Unit>>
+            public MedicalHistory MedicalHistory { get; set; }
+        
+        }
+
+        /// <summary>
+        /// Validator for the MedicalHistoryCreate command.
+        /// </summary>
+        public class CommandValidator : AbstractValidator<Command>
+        {
+            public CommandValidator()
             {
-                /// <summary>
-                /// Gets or sets the medical history to be created.
-                /// </summary>
-                public MedicalHistory MedicalHistory { get; set; }
-            }
-
-            public class CommandValidator : AbstractValidator<Command>
-            {
-                public CommandValidator()
-                {
-                    RuleFor(x => x.MedicalHistory).SetValidator(new MedicalHistoryValidators());
-                }
-            }
-
-            /// <summary>
-            /// Represents the handler for the <see cref="Command"/> to create a new medical history.
-            /// </summary>
-            public class Handler : IRequestHandler<Command, Result<Unit>>
-            {
-                private readonly ApplicationDbContext _context;
-
-                /// <summary>
-                /// Initializes a new instance of the <see cref="Handler"/> class with the specified application database context.
-                /// </summary>
-                /// <param name="context">The application database context.</param>
-                public Handler(ApplicationDbContext context)
-                {
-                    _context = context;
-                }
-
-                /// <summary>
-                /// Handles the command to create a new medical history.
-                /// </summary>
-                /// <param name="request">The command representing the medical history to be created.</param>
-                /// <param name="cancellationToken">The cancellation token.</param>
-                /// <returns>A task representing the asynchronous operation. The task result is a <see cref="Unit"/>.</returns>
-                public async Task<Result<Unit>> Handle(Command request, CancellationToken cancellationToken)
-                {
-                //request.MedicalHistory.CreatedAt = DateTime.UtcNow;
-                //request.MedicalHistory.UpdatedAt = DateTime.UtcNow;
-
-                //_context.MedicalHistories.Add(request.MedicalHistory);
-
-                //var result = await _context.SaveChangesAsync() > 0;
-
-                //if (!result) return Result<Unit>.Failure("Failed to create new Appointment");
-
-                //return Result<Unit>.Success(Unit.Value);
-
-                var patient = await _context.Patients
-                    .Include(p => p.User)
-                    .SingleOrDefaultAsync(p => p.PatientId == request.MedicalHistory.PatientId, cancellationToken);
-
-                if (patient == null)
-                    return Result<Unit>.Failure("Patient not found");
-
-                request.MedicalHistory.CreatedAt = DateTime.UtcNow;
-                request.MedicalHistory.UpdatedAt = DateTime.UtcNow;
-
-                request.MedicalHistory.PatientName = patient.User.DisplayName;
-
-                _context.MedicalHistories.Add(request.MedicalHistory);
-
-                var result = await _context.SaveChangesAsync() > 0;
-
-                if (!result)
-                    return Result<Unit>.Failure("Failed to create new medical history");
-
-                return Result<Unit>.Success(Unit.Value);
-            }
+                RuleFor(x => x.MedicalHistory).SetValidator(new MedicalHistoryValidators());
             }
         }
 
+        /// <summary>
+        /// Handler for processing the MedicalHistoryCreate command.
+        /// </summary>
+        public class Handler(ApplicationDbContext context) : IRequestHandler<Command, Result<Unit>>
+        {
+            /// <summary>
+            /// Handles the MedicalHistoryCreate command.
+            /// </summary>
+            /// <param name="request">The command request.</param>
+            /// <param name="cancellationToken">The cancellation token.</param>
+            /// <returns>A result indicating success or failure of the create operation.</returns>
+            public async Task<Result<Unit>> Handle(Command request, CancellationToken cancellationToken)
+            {
+                try
+                {
+                    var patient = await context.Patients
+                    .Include(p => p.User)
+                    .SingleOrDefaultAsync(p => p.PatientId == request.MedicalHistory.PatientId, cancellationToken);
+
+                    if (patient is null) return Result<Unit>.Failure("Patient not found");
+
+                    request.MedicalHistory.PatientName = patient.User.DisplayName;
+
+                    context.MedicalHistories.Add(request.MedicalHistory);
+
+                    var result = await context.SaveChangesAsync(cancellationToken) > 0;
+
+                    if (!result) return Result<Unit>.Failure("Failed to create new medical history");
+
+                    return Result<Unit>.Success(Unit.Value);
+                }               
+                catch (Exception ex)
+                {
+                    return Result<Unit>.Failure(ex.Message);
+                }
+            }
+        }
     }
+
+}
