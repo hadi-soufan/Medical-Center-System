49760020db4589cb042ce282a5a88c648e5b49a1
Hadi Soufan <hadisoufan74@gmail.com>
2024-03-09 07:17:16 +0200

    Represents receptionist entity.

---
+++ PMSApi/Domain/Entities/Receptionist.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Domain.Entities
+{
+    /// <summary>
+    /// Represents receptionist entity.
+    /// </summary>
+    public class Receptionist
+    {
+        public Guid ReceptionistId { get; set; }
+
+        public string UserId { get; set; }
+        [ForeignKey("UserId")]
+        public AppUser User { get; set; }
+
+        public DateOnly CreatedAt { get; set; } = DateOnly.FromDateTime(DateTime.UtcNow);
+        public DateOnly UpdatedAt { get; set; }
+        public bool IsDeleted { get; set; } = false;
+    }
+}
