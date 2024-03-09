959067853986d221a571c4ebd0b51bb237e660ee
Hadi Soufan <hadisoufan74@gmail.com>
2024-03-09 07:05:59 +0200

    Represents an application exception.

--- PMSApi/Application/Core/AppException.cs
+++ PMSApi/Application/Core/AppException.cs
@@ -1,7 +1,16 @@
 ﻿namespace Application.Core
 {
+    /// <summary>
+    /// Represents an application exception.
+    /// </summary>
     public class AppException
     {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="AppException"/> class.
+        /// </summary>
+        /// <param name="statusCode">The HTTP status code associated with the exception.</param>
+        /// <param name="message">The error message.</param>
+        /// <param name="details">Optional additional details about the exception.</param>
         public AppException(int statusCode, string message, string details = null)
         {
             StatusCode = statusCode;
