387d7bb8ca00a8f03bc61239547aa3a8b051acf9
Hadi Soufan <hadisoufan74@gmail.com>
2024-03-09 07:14:53 +0200

    Represents accountant entity.

---
+++ PMSApi/Domain/Entities/Accountant.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Domain.Entities
+{
+    /// <summary>
+    /// Represents accountant entity.
+    /// </summary>
+    public class Accountant
+    {
+        public Guid AccountantId { get; set; }
+        public string UserId { get; set; }
+
+        [ForeignKey("UserId")]
+        public AppUser User { get; set; }
+
+        public DateOnly CreatedAt { get; set; } = DateOnly.FromDateTime(DateTime.UtcNow);
+        public DateOnly  UpdatedAt { get; set; }
+
+        public bool IsDeleted { get; set; } = false;
+    }
+}
