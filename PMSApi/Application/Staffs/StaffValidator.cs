e7a7e7c5698cbe19213495fa5621ccf4b905d181
Hadi Soufan <hadisoufan74@gmail.com>
2024-03-09 07:13:51 +0200

    Validator for the StaffDto class.

---
+++ PMSApi/Application/Staffs/StaffValidator.cs
@@ -0,0 +1,26 @@
+﻿using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Staffs
+{
+    /// <summary>
+    /// Validator for the StaffDto class.
+    /// </summary>
+    public class StaffValidator : AbstractValidator<StaffDto> 
+    {
+        public StaffValidator() 
+        {
+            RuleFor(x => x.Email).NotEmpty();
+            RuleFor(x => x.PhoneNumber).NotEmpty();
+            RuleFor(x => x.Address).NotEmpty();
+            RuleFor(x => x.City).NotEmpty();
+            RuleFor(x => x.ZipCode).NotEmpty();
+            RuleFor(x => x.Occupation).NotEmpty();
+            RuleFor(x => x.State).NotEmpty();
+        }
+    }
+}
