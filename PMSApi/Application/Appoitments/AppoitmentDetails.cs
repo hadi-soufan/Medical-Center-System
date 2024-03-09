32e7c3e247bc8ffae09dccf78626d2d7657f3849
Hadi Soufan <hadisoufan74@gmail.com>
2024-03-09 07:05:32 +0200

    Provides functionality to retrieve details of an appointment.

--- PMSApi/Application/Appoitments/AppoitmentDetails.cs
+++ PMSApi/Application/Appoitments/AppoitmentDetails.cs
@@ -1,56 +1,40 @@
 ﻿using Application.Appoitments;
 using Application.Core;
-using Domain.Entities;
 using MediatR;
 using Microsoft.EntityFrameworkCore;
 using Persistence;
-using System;
-using System.Threading;
-using System.Threading.Tasks;
 
 namespace Application.Appointments
 {
     /// <summary>
-    /// Represents a query to retrieve details of an appointment.
+    /// Provides functionality to retrieve details of an appointment.
     /// </summary>
     public class AppointmentDetails
     {
         /// <summary>
-        /// Represents the query to retrieve details of an appointment.
+        /// Query to retrieve details of an appointment.
         /// </summary>
         public class Query : IRequest<Result<AppointmentDto>>
         {
-            /// <summary>
-            /// Gets or sets the ID of the appointment for which details are to be retrieved.
-            /// </summary>
             public Guid Id { get; set; }
         }
 
         /// <summary>
-        /// Represents the handler for the <see cref="Query"/> to retrieve details of an appointment.
+        /// Handler to process the AppointmentDetails query.
         /// </summary>
-        public class Handler : IRequestHandler<Query, Result<AppointmentDto>>
+        public class Handler(ApplicationDbContext context) : IRequestHandler<Query, Result<AppointmentDto>>
         {
-            private readonly ApplicationDbContext _context;
-
             /// <summary>
-            /// Initializes a new instance of the <see cref="Handler"/> class with the specified application database context.
+            /// Handles the AppointmentDetails query.
             /// </summary>
-            /// <param name="context">The application database context.</param>
-            public Handler(ApplicationDbContext context)
-            {
-                _context = context;
-            }
-
-            /// <summary>
-            /// Handles the query to retrieve details of an appointment.
-            /// </summary>
-            /// <param name="request">The query representing the request to retrieve details of an appointment.</param>
+            /// <param name="request">The details query.</param>
             /// <param name="cancellationToken">The cancellation token.</param>
-            /// <returns>A task representing the asynchronous operation. The task result is the appointment details.</returns>
+            /// <returns>A result containing details of the appointment.</returns>
             public async Task<Result<AppointmentDto>> Handle(Query request, CancellationToken cancellationToken)
             {
-                var appointment = await _context.Appointments
+                try
+                {
+                    var appointment = await context.Appointments
                     .Include(a => a.Patient)
                     .Select(a => new AppointmentDto
                     {
@@ -64,14 +48,13 @@ namespace Application.Appointments
                     })
                     .FirstOrDefaultAsync(a => a.AppointmentId == request.Id, cancellationToken);
 
-                if (appointment == null)
-                {
-                    return Result<AppointmentDto>.Failure("Appointment not found");
+                    if (appointment is null) return Result<AppointmentDto>.Failure("Appointment not found");
+                    return Result<AppointmentDto>.Success(appointment);
                 }
-
-                
-
-                return Result<AppointmentDto>.Success(appointment);
+                catch (Exception ex)
+                {
+                    return Result<AppointmentDto>.Failure(ex.Message);
+                }
             }
         }
     }
