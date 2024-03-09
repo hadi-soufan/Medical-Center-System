e43772d45947efacaf5be6e2bfa4c10ccb715607
Hadi Soufan <hadisoufan74@gmail.com>
2024-03-09 07:06:49 +0200

    Provides functionality to retrieve details of a doctor.

--- PMSApi/Application/Doctors/DoctorDetails.cs
+++ PMSApi/Application/Doctors/DoctorDetails.cs
@@ -1,69 +1,55 @@
-﻿using Application.Appoitments;
-using Application.Core;
+﻿using Application.Core;
+using AutoMapper;
 using Domain.Entities;
 using MediatR;
 using Microsoft.EntityFrameworkCore;
 using Persistence;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Application.Doctors
 {
+    /// <summary>
+    /// Provides functionality to retrieve details of a doctor.
+    /// </summary>
     public class DoctorDetails
     {
+        /// <summary>
+        /// Query to retrieve details of a doctor.
+        /// </summary>
         public class Query: IRequest<Result<DoctorDto>>
         {
             public Guid Id { get; set; }
         }
 
-        public class Handler : IRequestHandler<Query, Result<DoctorDto>>
+        /// <summary>
+        /// Handler to process the DoctorDetails query.
+        /// </summary>
+        public class Handler(ApplicationDbContext context, IMapper mapper) : IRequestHandler<Query, Result<DoctorDto>>
         {
-            private readonly ApplicationDbContext _context;
-
-            public Handler(ApplicationDbContext context)
-            {
-                _context = context;
-            }
-
+            /// <summary>
+            /// Handles the DoctorDetails query.
+            /// </summary>
+            /// <param name="request">The details query.</param>
+            /// <param name="cancellationToken">The cancellation token.</param>
+            /// <returns>A result containing details of the doctor.</returns>
             public async Task<Result<DoctorDto>> Handle(Query request, CancellationToken cancellationToken)
             {
-                var doctor = await _context.Doctors
+                try
+                {
+                    var doctor = await context.Doctors
                     .Include(d => d.User)
+                    .Where(d => !d.IsDeleted)
                     .FirstOrDefaultAsync(d => d.DoctorId == request.Id, cancellationToken);
 
-                if (doctor == null)
-                {
-                    return Result<DoctorDto>.Failure("Doctor not found");
+                    if (doctor is null) return Result<DoctorDto>.Failure("Doctor not found");
+
+                    var doctorDto = mapper.Map<Doctor, DoctorDto>(doctor);
+
+                    return Result<DoctorDto>.Success(doctorDto);
                 }
-
-                var doctorDto = new DoctorDto
+                catch (Exception ex)
                 {
-                    DoctorId = doctor.DoctorId,
-                    DisplayName = doctor.User.DisplayName,
-                    Email = doctor.User.Email,
-                    Username = doctor.User.UserName,
-                    PhoneNumber = doctor.User.PhoneNumber,
-                    FatherName = doctor.User.FatherName,
-                    MotherName = doctor.User.MotherName,
-                    DateOfBirth = doctor.User.DateOfBirth,
-                    Nationality = doctor.User.Nationality,
-                    Education = doctor.User.Education,
-                    Gender = doctor.User.Gender,
-                    MaritalStatus = doctor.User.MaritalStatus,
-                    BloodType = doctor.User.BloodType,
-                    Address = doctor.User.Address,
-                    City = doctor.User.City,
-                    ZipCode = doctor.User.ZipCode,
-                    State = doctor.User.State,
-                    Occupation = doctor.User.Occupation,
-                    InsuranceId = doctor.User.InsuranceId,
-                    DoctorLicenseId = doctor.DoctorLicenseId
-                };
-
-                return Result<DoctorDto>.Success(doctorDto);
+                    return Result<DoctorDto>.Failure(ex.Message);
+                }
             }
 
         }
