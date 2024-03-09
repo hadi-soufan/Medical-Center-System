59d70df1421a1324f7edf6b0e37ece59074efdd0
Hadi Soufan <hadisoufan74@gmail.com>
2024-03-09 07:12:06 +0200

    Data transfer object for receptionist information.

---
+++ PMSApi/Application/Receptionists/ReceptionistDto.cs
@@ -0,0 +1,60 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Application.Receptionist
+{
+    /// <summary>
+    /// Data transfer object for receptionist information.
+    /// </summary>
+    public class ReceptionistDto
+    {
+        public Guid ReceptionistId { get; set; }
+
+        [StringLength(150)]
+        public string DisplayName { get; set; }
+
+        [StringLength(150)]
+        public string Username { get; set; }
+
+        [StringLength(150)]
+        public string Email { get; set; }
+
+        [StringLength(150)]
+        public string PhoneNumber { get; set; }
+
+        [StringLength(150)]
+        public string FatherName { get; set; }
+
+        [StringLength(225)]
+        public string MotherName { get; set; }
+        public DateTime DateOfBirth { get; set; }
+
+        [StringLength(150)]
+        public string Nationality { get; set; }
+
+        [StringLength(150)]
+        public string Education { get; set; }
+
+        [StringLength(6)]
+        public string Gender { get; set; }
+
+        [StringLength(50)]
+        public string MaritalStatus { get; set; }
+
+        [StringLength(10)]
+        public string BloodType { get; set; }
+
+        [StringLength(255)]
+        public string Address { get; set; }
+
+        [StringLength(255)]
+        public string City { get; set; }
+        public int ZipCode { get; set; }
+
+        [StringLength(255)]
+        public string State { get; set; }
+
+        [StringLength(255)]
+        public string Occupation { get; set; }
+        public int InsuranceId { get; set; }
+    }
+}
