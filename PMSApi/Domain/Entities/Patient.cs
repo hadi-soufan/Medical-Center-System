8738c295924ed5c8c2ea46f78e73fffc5c117681
Hadi Soufan <hadisoufan74@gmail.com>
2024-03-09 07:16:41 +0200

    Represents patient entity.

--- PMSApi/Domain/Entities/Patient.cs
+++ PMSApi/Domain/Entities/Patient.cs
@@ -13,6 +13,10 @@ namespace Domain.Entities
         [ForeignKey("UserId")]
         public AppUser User { get; set; }
 
+        public DateOnly CreatedAt { get; set; } = DateOnly.FromDateTime(DateTime.UtcNow);
+        public DateOnly UpdatedAt { get; set; }
+        public bool IsDeleted { get; set; } = false;
+
         public ICollection<Appointment> Appointments { get; set; }
 
     }
