52b9e16ccb03d4826cd25d791209dd9277610073
Hadi Soufan <hadisoufan74@gmail.com>
2024-03-09 07:04:17 +0200

    Represents the data transfer object for an accountant.

---
+++ PMSApi/Application/Accountants/AccoutantDto.cs
@@ -0,0 +1,61 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Application.Accountants
+{
+    /// <summary>
+    /// Represents the data transfer object for an accountant.
+    /// </summary>
+    public class AccoutantDto
+    {
+        public Guid AccountantId { get; set; }
+
+        [StringLength(150)]
+        public string DisplayName { get; set; }
+
+        [StringLength(150)]
+        public string Username { get; set; }
+
+        [StringLength(150)]
+        public string Email { get; set; }
+
+        [StringLength(150)]
+        public string PhoneNumber { get; set; }
+
+        [StringLength(150)]
+        public string FatherName { get; set; }
+
+        [StringLength(225)]
+        public string MotherName { get; set; }
+        public DateTime DateOfBirth { get; set; }
+
+        [StringLength(150)]
+        public string Nationality { get; set; }
+
+        [StringLength(150)]
+        public string Education { get; set; }
+
+        [StringLength(6)]
+        public string Gender { get; set; }
+
+        [StringLength(50)]
+        public string MaritalStatus { get; set; }
+
+        [StringLength(10)]
+        public string BloodType { get; set; }
+
+        [StringLength(255)]
+        public string Address { get; set; }
+
+        [StringLength(255)]
+        public string City { get; set; }
+        public int ZipCode { get; set; }
+
+        [StringLength(255)]
+        public string State { get; set; }
+
+        [StringLength(255)]
+        public string Occupation { get; set; }
+        public int InsuranceId { get; set; }
+
+    }
+}
