1f5a26b85d5a312dd7dbafcc8299948c1a07c55b
Hadi Soufan <hadisoufan74@gmail.com>
2024-03-09 07:15:37 +0200

    Represents doctor entity.

--- PMSApi/Domain/Entities/Doctor.cs
+++ PMSApi/Domain/Entities/Doctor.cs
@@ -2,6 +2,9 @@
 
 namespace Domain.Entities
 {
+    /// <summary>
+    /// Represents doctor entity.
+    /// </summary>
     public class Doctor
     {
         public Guid DoctorId { get; set; }
@@ -9,6 +12,9 @@ namespace Domain.Entities
         public string UserId { get; set; }
         [ForeignKey("UserId")]  
         public AppUser User { get; set; }
+        public DateOnly CreatedAt { get; set; } = DateOnly.FromDateTime(DateTime.UtcNow);
+        public DateOnly UpdatedAt { get; set; }
+        public bool IsDeleted { get; set; } = false;
 
         public ICollection<Appointment> Appointments { get; set; }
 
