a7938e0ae769397105a5bb51f2e4030a75ab80a7
Hadi Soufan <hadisoufan74@gmail.com>
2024-03-09 07:16:22 +0200

    Represents nurse entity

---
+++ PMSApi/Domain/Entities/Nurse.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Domain.Entities
+{
+    /// <summary>
+    /// Represents nurse entity.
+    /// </summary>
+    public class Nurse
+    {
+        public Guid NurseId { get; set; }
+        public int NurseLicenseId { get; set; }
+        public string UserId { get; set; }
+
+        [ForeignKey("UserId")]
+        public AppUser User { get; set; }
+
+        public DateOnly CreatedAt { get; set; } = DateOnly.FromDateTime(DateTime.UtcNow);
+        public DateOnly UpdatedAt { get; set; }
+        public bool IsDeleted { get; set; } = false;
+    }
+}
