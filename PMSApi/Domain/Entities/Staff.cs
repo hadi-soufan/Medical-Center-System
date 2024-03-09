b2e84f28c334dc9c3f0a1015ac05dd3633b41d3d
Hadi Soufan <hadisoufan74@gmail.com>
2024-03-09 07:17:50 +0200

    Represents staff entity.

---
+++ PMSApi/Domain/Entities/Staff.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Entities
+{
+    public class Staff
+    {
+        public Guid StaffId { get; set; }
+        public string UserId { get; set; }
+        [ForeignKey("UserId")]
+        public AppUser User { get; set; }
+
+        public DateOnly CreatedAt { get; set; } = DateOnly.FromDateTime(DateTime.UtcNow);
+        public DateOnly UpdatedAt { get; set; }
+        public bool IsDeleted { get; set; } = false;
+    }
+}
