7ab72626f6bcc2f95e0529193ae05a9cce375956
Hadi Soufan <hadisoufan74@gmail.com>
2024-03-09 07:03:48 +0200

    Validates properties of the AccountantDto class.

---
+++ PMSApi/Application/Accountants/AccountantValidator.cs
@@ -0,0 +1,21 @@
+﻿using FluentValidation;
+
+namespace Application.Accountants
+{
+    /// <summary>
+    /// Validates properties of the AccountantDto class.
+    /// </summary>
+    public class AccountantValidator : AbstractValidator<AccoutantDto>
+    {
+        public AccountantValidator()
+        {
+            RuleFor(x => x.Email).NotEmpty();
+            RuleFor(x => x.PhoneNumber).NotEmpty();
+            RuleFor(x => x.Address).NotEmpty();
+            RuleFor(x => x.City).NotEmpty();
+            RuleFor(x => x.ZipCode).NotEmpty();
+            RuleFor(x => x.Occupation).NotEmpty();
+            RuleFor(x => x.State).NotEmpty();
+        }
+    }
+}
