6fece644ae51527511267d7565fc894b5c5dea0c
Hadi Soufan <hadisoufan74@gmail.com>
2024-03-09 07:09:17 +0200

    Represents a validator for the MedicalHistory entity.

--- PMSApi/Application/MedicalHistoreis/MedicalHistoryValidators.cs
+++ PMSApi/Application/MedicalHistoreis/MedicalHistoryValidators.cs
@@ -1,16 +1,14 @@
 ﻿using Domain.Entities;
 using FluentValidation;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Application.MedicalHistoreis
 {
+    /// <summary>
+    /// Represents a validator for the MedicalHistory entity.
+    /// </summary>
     public class MedicalHistoryValidators : AbstractValidator<MedicalHistory>
     {
-        public MedicalHistoryValidators() 
+        public MedicalHistoryValidators()
         {
             RuleFor(x => x.Height).NotEmpty();
             RuleFor(x => x.Weight).NotEmpty();
