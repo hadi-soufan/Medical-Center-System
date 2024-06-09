d0f3b5864698326f3b9822cd643b9ee0ebbfa60b
Hadi Soufan <hadisoufan74@gmail.com>
2024-06-09 19:47:24 +0300

    New migrations

---
+++ PMSApi/Persistence/Migrations/20240504155421_PatientEntityEditedd.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class PatientEntityEditedd : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_MedicalHistories_PatientId",
+                table: "MedicalHistories");
+
+            migrationBuilder.AddColumn<string>(
+                name: "MedicalHistoryId",
+                table: "Patients",
+                type: "text",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_MedicalHistories_PatientId",
+                table: "MedicalHistories",
+                column: "PatientId",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_MedicalHistories_PatientId",
+                table: "MedicalHistories");
+
+            migrationBuilder.DropColumn(
+                name: "MedicalHistoryId",
+                table: "Patients");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_MedicalHistories_PatientId",
+                table: "MedicalHistories",
+                column: "PatientId");
+        }
+    }
+}
